fc7a4e04828d75cc95a132d49da9721362dd5aba
colins44 <colin.pringlewood@gmail.com>
2015-02-16 22:21:56 +0000

    now getting flight data from all london airports

--- flights/management/commands/get_routes.py
+++ flights/management/commands/get_routes.py
@@ -7,70 +7,33 @@ from time import sleep
 
 # def build_route(self, content_type, object_id, ):
 
+london_airports = ['LHR','LGW','STN','LCY','LTN']
 
 class Command(BaseCommand):
     help = 'Creates the routes to and from each airport'
 
     def handle(self, *args, **options):
-        for flight in outbound_flights['scheduledFlights']:
 
-            departure_airport = Airport.objects.get_or_create(code=flight['departureAirportFsCode'])
-            arrival_airport = Airport.objects.get_or_create(code=flight['arrivalAirportFsCode'])
+        for x in london_airports:
+            london_airport = Airport.objects.get(code=x)
+            airports = Airport.objects.exclude(code=outbound_flights['scheduledFlights'][0]['departureAirportFsCode'])
 
-        for flight in outbound_flights['scheduledFlights']:
-            try:
-                departure_date = datetime.strptime(flight['departureTime'], '%Y-%m-%dT%H:%M:%S.000')
-                arrival_date = datetime.strptime(flight['arrivalTime'], '%Y-%m-%dT%H:%M:%S.000')
-                departure_airport = Airport.objects.get(code=flight['departureAirportFsCode'])
-                arrival_airport = Airport.objects.get(code=flight['arrivalAirportFsCode'])
-                Flight.objects.create(departure_airport=departure_airport,
-                                      arrival_airport=arrival_airport,
-                                      departure_time=departure_date,
-                                      flight_no= flight['flightNumber'],
-                                      carrier_code= flight['carrierFsCode'],
-                                      arrival_time= arrival_date,
-                                      stops= flight['stops'],)
-            except ObjectDoesNotExist:
-                print 'we could not find your airport, or more than one airport was returned'
-
-        for flight in inbound_flights['scheduledFlights']:
-            departure_airport = Airport.objects.get_or_create(code=flight['departureAirportFsCode'],)
-            arrival_airport = Airport.objects.get_or_create(code=flight['arrivalAirportFsCode'])
-
-        for flight in inbound_flights['scheduledFlights']:
-            try:
-                departure_date = datetime.strptime(flight['departureTime'], '%Y-%m-%dT%H:%M:%S.000')
-                arrival_date = datetime.strptime(flight['arrivalTime'], '%Y-%m-%dT%H:%M:%S.000')
-                departure_airport = Airport.objects.get(code=flight['departureAirportFsCode'])
-                arrival_airport = Airport.objects.get(code=flight['arrivalAirportFsCode'])
-                Flight.objects.create(departure_airport=departure_airport,
-                                      arrival_airport=arrival_airport,
-                                      departure_time=departure_date,
-                                      flight_no= flight['flightNumber'],
-                                      carrier_code= flight['carrierFsCode'],
-                                      arrival_time= arrival_date,
-                                      stops= flight['stops'],)
-            except ObjectDoesNotExist:
-                print 'we could not find your airport, or more than one airport was returned'
-
-        airports = Airport.objects.exclude(code=outbound_flights['scheduledFlights'][0]['departureAirportFsCode'])
-
-        for airport in airports:
-            #this is the airport to which the flight is going to
-            airport_inbound_flights = Flight.objects.filter(departure_airport=airport)
-            airport_outbound_flights = Flight.objects.filter(arrival_airport=airport)
+            for airport in airports:
+                #this is the airport to which the flight is going to
+                airport_inbound_flights = Flight.objects.filter(departure_airport=airport, arrival_airport=london_airport)
+                airport_outbound_flights = Flight.objects.filter(arrival_airport=airport, departure_airport=london_airport)
 
 
-            #get or create round trip
-            #this is the airport to which the flight is going to
-            route = Route(airport = airport)
-            route.save()
-            if len(airport_outbound_flights) != 0:
-                route.outbound_flights.add(airport_outbound_flights[0])
+                #get or create round trip
+                #this is the airport to which the flight is going to
+                route = Route(airport = airport)
+                route.save()
+                if len(airport_outbound_flights) != 0:
+                    route.outbound_flights.add(airport_outbound_flights[0])
 
-            if len(airport_inbound_flights) != 0:
-                route.inbound_flights.add(airport_inbound_flights[0])
-            route.save()
+                if len(airport_inbound_flights) != 0:
+                    route.inbound_flights.add(airport_inbound_flights[0])
+                route.save()
 
 
 
