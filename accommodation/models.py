04ea0a4689c41d5a054ed269cab4f2dec47886ad
Colin Pringle-Wood <colin.pringlewood@gmail.com>
2015-03-22 17:52:31 +0000

    trying to save accomo

--- accommodation/models.py
+++ accommodation/models.py
@@ -1,13 +1,41 @@
 from django.db import models
-from location.models import City
+from location.models import City, Currency
 
-ACCOMMODATION_CHOICES =(('hotel', 'hotel'),
-                        ('hostel', 'hostel'))
+
+ACCOMMODATION_CHOICES =(('1', 'hotel'),
+                        ('5', 'bed and breakfast'),)
 
 class Accommodation(models.Model):
     city = models.ForeignKey(City)
     type = models.CharField(max_length=100, choices=ACCOMMODATION_CHOICES)
     name = models.CharField(max_length=150)
+    address1 = models.CharField(max_length=150, blank=True)
+    address2 = models.CharField(max_length=150, blank=True)
+    post_code = models.CharField(max_length=50, blank=True)
+    rating = models.FloatField(default=3)
+    high_rate = models.IntegerField(blank=False, null=True)
+    low_rate = models.IntegerField(blank=True, )
+    ean_hotel_id = models.IntegerField(blank=True, null=True)
 
     def __unicode__(self):
         return '%s %s in %s' % (self.name, self.type, self.city)
+
+class RoomRate(models.Model):
+    hotel = models.ForeignKey(Accommodation)
+    max_occupancy = models.IntegerField(blank=True, null=True)
+    quoted_occupancy = models.IntegerField(blank=True, null=True)
+    room_description = models.CharField(max_length=150, blank=True, null=True)
+    room_type_code = models.IntegerField(blank=True, null=True)
+    rate_code = models.IntegerField(blank=True, null=True)
+    arrival_date = models.DateField()
+    departure_date = models.DateField()
+    no_of_adults = models.IntegerField(blank=True, null=True)
+    no_of_children = models.IntegerField(blank=True, null=True)
+    promotion = models.BooleanField(default=False)
+    deeplink = models.URLField(blank=True, null=True)
+    total = models.FloatField(blank=True, null=True)
+    rate_type = models.CharField(max_length=150, blank=True, null=True)
+    currency = models.ForeignKey(Currency)
+
+    def __unicode__(self):
+        return self.hotel
--- accommodation/utils.py
+++ accommodation/utils.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 
 EAN_HOTEL_API  ={
@@ -17,8 +18,8 @@ currencyCode='GBP'
 city='london'
 countryCode='UK'
 #american dates mm/dd/yyyy
-arrivalDate='03/19/2015'
-departureDate='03/21/2015'
+arrivalDate='03/27/2015'
+departureDate='03/29/2015'
 room1="2"
 
 
@@ -27,45 +28,39 @@ q =''
 url = 'http://dev.api.ean.com/ean-services/rs/hotel/v3/list?apiKey=%s&cid=%s&customerIpAddress=%s&customerUserAgent=%s&customerSessionId=%s&minorRev=%s&locale=%s&currencyCode=%s&city=%s&countryCode=%s&arrivalDate=%s&departureDate=%s&room1=%s' % (api_key, cid, customerIpAddress, customerUserAgent, customerSessionId, minorRev, locale, currencyCode, city, countryCode, arrivalDate, departureDate, room1)
 
 print url
-#
-# def build_url(xml):
-#     url = 'http://dev.api.ean.com/ean-services/rs/hotel/v3/list?api_key=%s&city=%s' % (EAN_HOTEL_API['key'], 'london')
-#     return url
-#
-# xml="""<HotelListRequest>
-# 	<city>London</city>
-# </HotelListRequest>"""
-#
-#
-# url = build_url(xml)
-#
-# r = requests.get(url)
-# print r.status_code
-# print r.content
+
+# print url
 
 
-#http://developer.ean.com/docs/hotel-list/examples/xml-basic-availability/
+eanurl = 'http://dev.api.ean.com/ean-services/rs/hotel/v3/list?apiKey=3rdyahz9hnfnba6nuqu8gedp&cid=55505'
 
 
-# http://api.ean.com/ean-services/rs/hotel/v3/list?
-# apiKey=#####
-# &cid=#####
-# &customerIpAddress=#####
-# &customerUserAgent=######
-# &customerSessionId=#####
-# &minorRev=##
-# &locale=en_US
-# &currencyCode=USD
-# &xml=
-# <HotelListRequest>
-# 	<city>Seattle</city>
-# 	<stateProvinceCode>WA</stateProvinceCode>
-# 	<countryCode>US</countryCode>
-# 	<arrivalDate>08/01/2015</arrivalDate>
-# 	<departureDate>08/03/2015</departureDate>
-# 	<RoomGroup>
-# 	  <Room>
-# 	    <numberOfAdults>2</numberOfAdults>
-# 	  </Room>
-# 	</RoomGroup>
-# </HotelListRequest>
+city='london'
+countryCode='UK'
+arrivalDate='09/19/2015'
+departureDate='09/21/2015'
+room1="2"
+ean_url = 'http://dev.api.ean.com/ean-services/rs/hotel/v3/list?apiKey=3rdyahz9hnfnba6nuqu8gedp&cid=55505&customerIpAddress=37.157.38.18&customerUserAgent=mobile_web&customerSessionId=somerandoneidentifyingstring&minorRev=28&locale=en_US&currencyCode=GBP&arrivalDate=03%2F27%2F2015&city=london&departureDate=03%2F29%2F2015&countryCode=UK&room1=2'
+
+
+payload = {
+'city':'london',
+# 'countryCode':'UK',
+'arrivalDate':'06/27/2015',
+'departureDate':'06/30/2015',
+'room1':"2",
+}
+#
+r = requests.get(ean_url, params=payload)
+print r.url
+print r.status_code
+data = json.loads(r.content)
+hotels =  data.get('HotelListResponse').get('HotelList').get('HotelSummary')
+for hotel in hotels:
+    # print hotel.get('name'), hotel.get('highRate'), hotel.get('lowRate')
+    # print '$$$$$$$$$$$'
+    room = hotel.get('RoomRateDetailsList').get('RoomRateDetails')
+    # print room
+    # print '$$$$$$$$$$'
+    rate = room.get('RateInfos').get('RateInfo').get('ChargeableRateInfo').get('@total')
+    print rate
