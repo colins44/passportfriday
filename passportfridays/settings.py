8c3f9791c6d6e2cef255013b5fd048099549632b
colins44 <colin.pringlewood@gmail.com>
2015-04-11 22:13:59 +0100

    takeout host name

--- passportfridays/settings.py
+++ passportfridays/settings.py
@@ -15,7 +15,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'HOST': 'db',
+        # 'HOST': 'db',
         'PORT': 5432,
     }
 }
